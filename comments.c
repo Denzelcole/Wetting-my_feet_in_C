7ecff01d67a1f08e5d7b050f6e27a9565a7f2336
Denzelcole <theocole4@gmail.com>
2022-12-03 12:03:22 +0100

    This just to show types of comments

---
+++ comments.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+        /*
+         *This is a comment section it's ignored by code
+         */
+
+        // This can also be used as a comment too
+        printf("Comments are ignored but used as a reference\n");
+        return (0);
+}
