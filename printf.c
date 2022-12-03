e4c7dab06d92d7a0892516d7d85a0cbcb6b86001
Denzelcole <theocole4@gmail.com>
2022-12-03 11:23:10 +0100

    printf testing

---
+++ printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * What is printf?
+ * Description:- have you thought the funtion printf does or means?
+ * It basically prints stuff to the screen "Lol"
+ */
+
+int main(void)
+{
+        printf("How many %s with learning C %0.2f\n", "times", 30.566); // carefull here just add space instead you will get error
+}
