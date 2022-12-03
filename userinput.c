2873f82d6313ddb0e0ca0b54961dafcbb7d36d39
Denzelcole <theocole4@gmail.com>
2022-12-03 12:30:11 +0100

    user input in numbers

---
+++ userinput.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+        int old;
+        printf("How old you dey?: \n");
+        scanf("%d", &old);
+        printf("Chai %d?\n", old);
+        printf("You don old oh.\n");
+
+        return (0);
+}
+// for using a string we allocate to the char e.g
